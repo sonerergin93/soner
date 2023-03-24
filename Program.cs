0b3efe9ee4461abe2c473d4bc6abb957303d25a5
sonerergin93 <47483856+sonerergin93@users.noreply.github.com>
2023-03-25 01:03:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.CognitiveServices.Speech;
+
+namespace SpeechToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = SpeechConfig.FromSubscription("YOUR_SUBSCRIPTION_KEY", "YOUR_REGION");
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                Console.WriteLine("Say something...");
+                var result = recognizer.RecognizeOnceAsync().Result;
+                Console.WriteLine($"Text: {result.Text}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
